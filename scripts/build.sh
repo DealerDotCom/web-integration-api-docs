f1c28ab423e4f5d58c1db64c6d7a537e6231875f
Danny-Sundaresan <Danny.Sundaresan@coxautoinc.com>
2021-05-20 17:39:49 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 cp $DOC_BASE_FOLDER/index.html.md /usr/src/app/source
 
 cd /usr/src/app/source
-bundle exec middleman build --clean
+bundle exec middleman build --verbose --clean
 #Getting back to home folder to allow relative doc_base_folder
 cd $GITHUB_WORKSPACE
 mv /usr/src/app/build $DOC_BASE_FOLDER
